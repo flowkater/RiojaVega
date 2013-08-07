e3935f71779d838805b20d353777af77033eb7f8
pickerss <shtjdgus1090@Noh-MacBook.local>
2013-08-07 18:35:40 +0900

    Sample Sorting

---
+++ dicontainer/pickers/Sort/src/QuickSort.java
@@ -0,0 +1,16 @@
+
+
+import java.util.Arrays;
+
+public class QuickSort implements Sortable {
+
+	@Override
+	public int[] sorting(int[] collection) {
+		
+		System.out.println("QuickSort run...");
+		
+		Arrays.sort(collection);
+		return collection;
+	}
+
+}
